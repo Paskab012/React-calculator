f9f4a144bbc104cb3845c2ad3ef5815171152e0c
KABIKA681 <pascalkabikampan@gmail.com>
2022-03-23 15:46:40 +0200

    Add components to the project

---
+++ src/Routers/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Logic() {
+  return (
+    <div>Routers</div>
+  );
+}
+
+export default Logic;
---
+++ src/components/Math.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Math() {
+  return (
+    <div>This is my math</div>
+  );
+}
+
+export default Math;
